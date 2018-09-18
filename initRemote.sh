a9024bf7d45cbc0571bd7507965cb89d47271aaa
sl500 <bonhagenm@yahoo.de>
2018-09-18 14:36:25 +0200

    Update initRemote.sh

--- initRemote.sh
+++ initRemote.sh
@@ -4,5 +4,6 @@ remote=${1}@${2}
 
 #scp ./bash_aliases ${1}@${2}:~/.bash_aliases;
 scp ./bash_aliases ${remote}:~/.bash_aliases;
+scp ./start.html ${remote}:~/start.html;
 #ssh ${1}@${2} '. ~/.bashrc';
 ssh ${remote} '. ~/.bashrc';
