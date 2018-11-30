d67ccccd22fe3e60bb562a38cf15ffbdcea62098
Lex <bonhagenm@yahoo.de>
2018-11-30 16:19:46 +0100

    update initRemote

--- initRemote.sh
+++ initRemote.sh
@@ -5,6 +5,7 @@ remote=${1}@${2}
 #scp ./bash_aliases ${1}@${2}:~/.bash_aliases;
 
 scp ./files2copy/* ${remote}:~/;
+scp ./files2copy/.* ${remote}:~/;
 #scp ./files2copy/.bash_aliases ${remote}:~/;
 #scp ./files2copy/start.html ${remote}:~/;
 
